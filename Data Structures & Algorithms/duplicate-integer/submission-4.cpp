db96ac23d2a3073199cdf4bbb20884941eef6094
ayanmomin313 <aayanmomin35@gmail.com>
2026-09-24 01:47:43 +0530

    Update: duplicate-integer - submission-4

--- Data Structures & Algorithms/duplicate-integer/submission-4.cpp
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool hasDuplicate(vector<int>& nums) {
-          sort(nums.begin(),nums.end());
-          for(int i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1]){
-                return true;
-            }
-          }
-
+        unordered_map<int,int> mp;
+        for (int i :nums){
+            mp[i]++;
+            if(mp[i]>1) return true;
+            
+        }
         return false;
     }
 };
