0f3dc44f13912fedd35a167b75b0b6f7c9d5ef56
ayanmomin313 <aayanmomin35@gmail.com>
2026-09-25 02:45:56 +0530

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> mp;
+
+for (char c : s) mp[c]++;
+for (char c : t) mp[c]--;
+
+for (auto [c, freq] : mp)
+    if (freq != 0) return false;
+
+return true;
+    }
+};
