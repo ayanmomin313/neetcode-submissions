4a185fcfb45b03841aa866b9e9b0f46420cbebb3
ayanmomin313 <aayanmomin35@gmail.com>
2026-09-25 02:45:04 +0530

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> mp1, mp2;
+
+for (char c : s) mp1[c]++;
+for (char c : t) mp2[c]++;
+
+return mp1 == mp2;
+    }
+};
